5ab250801507d517cfc4cd856c474b931f30b0cd
Ivo Luis <ivoluis239@gmail.com>
2025-05-28 21:56:05 -0300

    Create options.lua

---
+++ options.lua
@@ -0,0 +1,22 @@
+-- init.lua básico mínimo
+
+vim.opt.number = true          -- mostra número das linhas
+
+vim.opt.relativenumber = true  -- linhas relativas
+
+vim.opt.expandtab = true       -- usa espaços no lugar de tab
+
+vim.opt.shiftwidth = 4         -- tamanho da indentação
+
+vim.opt.tabstop = 4            -- tab = 4 espaços
+
+vim.opt.cursorline = true      -- destaca linha do cursor
+
+vim.opt.wrap = false           -- não quebra linha visualmente
+
+vim.opt.ignorecase = true      -- busca case-insensitive
+
+vim.opt.smartcase = true       -- busca case-sensitive se usar maiúscula
+
+vim.opt.clipboard = "unnamedplus" -- usa clipboard do sistema
+
